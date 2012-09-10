20a3a9b3b68d9c631053c30ee15dd8db11d71023
Tim Dettrick <t.dettrick@uq.edu.au>
2012-09-10 11:19:34 +1000

    Adding defined content for SRU controller test.

--- spec/controllers/sru_controller_spec.rb
+++ spec/controllers/sru_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+require 'idzebra'
+
 describe SruController do
 
   def run_server
@@ -23,6 +25,25 @@ describe SruController do
     end
   end
 
+  before(:each) do
+    IdZebra::log_level = :warn
+    IdZebra::API('config/zebra/zebra.cfg') do |repo|
+      person = StaffPerson.new
+      person.assign_attributes({
+          :staff_id => "98773",
+          :last_name_mixed => "Atkins",
+          :first_names => "Thomas Francis",
+          :preferred_name => "Tommy",
+          :title => "Mr",
+          :email => "t.atkins@uq.edu.au"
+        }, :without_protection => true)
+      person.save
+      repo.init
+      repo.add_record(person.to_rif)
+      repo.commit
+    end
+  end
+
   it "should relay queries to a zebra UNIX socket" do
     sru_params = {
       'version' => '1.1',
@@ -31,9 +52,12 @@ describe SruController do
     }
     run_server do
       get :proxy, sru_params
-      response.code.should == "200"
-      response.body.should \
-        match(/<zs:numberOfRecords>\d+<\/zs:numberOfRecords>/)
+      response.code.to_i.should == 200
+      puts response.body
+      response.body.should satisfy do |xml|
+        xml =~ /<zs:numberOfRecords>(\d+)<\/zs:numberOfRecords>/
+      end
+      $1.to_i.should == 1
     end
   end
 
