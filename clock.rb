1a6edabf055fd395b542adb474d84e61ea1d5845
Tim Dettrick <t.dettrick@uq.edu.au>
2012-09-27 15:56:02 +1000

    Switching import back to one-at-a-time, as memory requirements were getting excessive.

--- clock.rb
+++ clock.rb
@@ -6,17 +6,15 @@ Dir.mkdir('tmp/data') rescue nil
 
 module Clockwork
 
-  every(15.minutes, 'output.records') do
+  every(6.hours, 'output.records') do
     models = [StaffPerson, Grant, GrantInvestigator, OrgUnit]
     IdZebra::log_level = :info
     IdZebra::API('config/zebra/zebra.cfg') do |repo|
       models.each do |model|
-        if model.respond_to?(:to_rif)
-          repo.add_record(model.to_rif)
-        else
-          model.all.each do |obj|
-            repo.add_record(obj.to_rif)
-          end
+        # While slower than generating a single document, the
+        # memory requirements are much less troublesome for a small VM.
+        model.all.each do |obj|
+          repo.add_record(obj.to_rif)
         end
         repo.commit
       end
